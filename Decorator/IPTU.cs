936106d459bcd418253dbfa1e4e79009a004f875
jefacb10 <32496187+jefacb10@users.noreply.github.com>
2021-05-12 19:22:12 -0300

    Add Decorator

---
+++ Decorator/IPTU.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreinamentoDesignPattern_Alura
+{
+    public class IPTU : Imposto
+    {
+        public IPTU(Imposto outroImposto) : base(outroImposto) { }
+        public IPTU() : base() { }
+
+        public override double Calcula(Orcamento orcamento)
+        {
+            return orcamento.Valor * 0.06 + CalculoDoOutroImposto(orcamento);
+        }
+    }
+}
